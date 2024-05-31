230cd97295a035619093bfbbf3bbae61c4d941e7
styxlab <you@example.com>
2024-05-31 16:15:51 +0200

    provoke error?

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,13 @@
-import { Link } from "@remix-run/react";
-import type { MetaFunction } from "@vercel/remix";
+import { UserStatus } from "@prisma/client";
 
 export const config = { runtime: 'edge' };
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix with Prisma</h1>
-      <ul>
-        <li>
-          <Link to="/prisma">Test page transition</Link>
-        </li>
-      </ul>
-    </div>
-  );
-}
+   const status = UserStatus.ACTIVE;
+    return (
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+        <h1>Prisma Test {status}</h1>
+      </div>
+    );
+  }
+  
