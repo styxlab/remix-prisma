dfd028f20d8cd4fadab6dd0bdbf5148866ba1d5f
styxlab <you@example.com>
2024-05-31 16:20:13 +0200

    latest

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,9 +5,7 @@ export const config = { runtime: 'edge' };
 export default function Index() {
    const status = UserStatus.ACTIVE;
     return (
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-        <h1>Prisma Test {status}</h1>
-      </div>
+      <h1>Prisma Test {status}</h1>
     );
   }
   
