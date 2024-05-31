faa233bfe3c565dfe824f78f7f56f8f6c4e1bf33
jj <csk@posteo.de>
2024-05-31 21:39:18 +0200

    test prefetch

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,25 @@
-import { UserStatus } from "@prisma/client";
+import { UserStatus } from "@prisma/client/edge";
+import { Link } from "@remix-run/react";
+import type { ReactNode } from "react";
 
-export const config = { runtime: 'edge' };
+function NavLink({ href, children }: { href: string; children: ReactNode }) {
+  return (
+    <Link prefetch='intent' to={href}>
+      {children}
+    </Link>
+  );
+}
+
+export const config = { runtime: "edge" };
 
 export default function Index() {
-   const status = UserStatus.ACTIVE;
-    return (
+  const status = UserStatus.ACTIVE;
+  return (
+    <>
       <h1>Prisma Test {status}</h1>
-    );
-  }
-  
+      <div>
+        <NavLink href='/contact'>Contact</NavLink>
+      </div>
+    </>
+  );
+}
